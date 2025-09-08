b85bd084c540bf34340e016b7520ede6baaac825
pedrohenry12 <faquinipedro12@gmail.com>
2025-09-08 09:59:18 -0300

    Update Contador.jsx

--- contador/src/components/Contador.jsx
+++ contador/src/components/Contador.jsx
@@ -1,22 +1,41 @@
+import { useState } from "react";
 import styles from "./Contador.module.css"
 
 export default function Contador() {
-  console.log(styles)
+  
+  const [contador, setcontador] = useState(0)
+
+  function incrementar(){
+    setcontador(contador +1)
+    
+  }
+
+  function decrementar(){
+    setcontador(contador -1)
+    
+  }
+
+  function zerar(){
+    setcontador(0)
+  }
   return(
     <div className={styles.container}>
     <h1 className={styles.titulo}>Contador</h1>
       <div className={styles.contadorDisplay}>
-        0
+        {contador}
       </div>
       <div className={styles.botaoContainer}>
-        <button className={styles.botao}>
+        <button className={styles.botao}
+        onClick={decrementar}>
           -
         </button>
-        <button className={styles.botao}>
+        <button className={styles.botao}
+        onClick={incrementar}>
           +
         </button>
       </div>
-      <button className={styles.botao}>
+      <button className={styles.botao}
+      onClick={zerar}>
         Reset
       </button>
     </div>
